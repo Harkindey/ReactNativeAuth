41f9c2a6638469b86dd118a4c424850e75c4f987
Emmanuel AKINDE <ibukunbamise@yahoo.com>
2017-04-22 14:03:09 +0100

    Authentication Flow

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,8 +1,21 @@
 import React, { Component } from 'react';
+import { Text } from 'react-native';
+import firebase from 'firebase';
 import { Button, Card, CardSection, Input } from './common';
 
 class LoginForm extends Component {
-  state = { email: ''};
+  state = { email: '', password: '', error:'' };
+
+  onButtonPress(){
+    const { email, password } = this.state;
+
+    firebase.auth().signInWithEmailAndPassword(email, password).catch(() => {
+      firebase.auth().createUserWithEmailAndPassword(email,password).catch(() => {
+        this.setState({ error: 'Authentication Failed.' });
+      });
+    });
+  }
+
     render() {
         return (
           <Card>
@@ -15,10 +28,22 @@ class LoginForm extends Component {
                 />
             </CardSection>
 
-            <CardSection />
+            <CardSection>
+              <Input
+                secureTextEntry
+                placeholder="password"
+                label="Password"
+                value={this.state.password}
+                onChangeText={password => this.setState({ password })}
+                />
+            </CardSection>
+
+            <Text style={styles.errorTextStyles}>
+              {this.state.error}
+            </Text>
 
           <CardSection>
-            <Button>
+            <Button onPress={this.onButtonPress.bind(this)}>
               Log In
             </Button>
           </CardSection>
@@ -26,4 +51,12 @@ class LoginForm extends Component {
         );
     }
 }
+
+const styles = {
+  errorTextStyles: {
+    fontSize: 20,
+    alignSelf: 'center',
+    color: 'red'
+  }
+};
 export default LoginForm;
