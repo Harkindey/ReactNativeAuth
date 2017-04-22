917773ec345c725078898088f75b1caaf952ec51
Emmanuel AKINDE <ibukunbamise@yahoo.com>
2017-04-22 09:28:40 +0100

    The LoginForm component

--- src/app.js
+++ src/app.js
@@ -1,17 +1,29 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View} from 'react-native';
+import firebase from 'firebase';
 import { Header } from './components/common';
+import LoginForm  from './components/LoginForm';
 
 
 class App extends Component {
-  render() {
-    return (
-      <View>
-        <Header headerText="Authentication" />
-        <Text>An Auth App!</Text>
-      </View>
-    );
-  }
+    componentWillMount() {
+        firebase.initializeApp({
+            apiKey: 'AIzaSyCNwDl0k5aisvXKkvTJN0R7b9aTAGIBycI',
+            authDomain: 'auth-b37ab.firebaseapp.com',
+            databaseURL: 'https://auth-b37ab.firebaseio.com',
+            projectId: 'auth-b37ab',
+            storageBucket: 'auth-b37ab.appspot.com',
+            messagingSenderId: '126662161514'
+        });
+    }
+    render() {
+        return (
+            <View>
+                <Header headerText="Authentication"/>
+                <LoginForm />
+            </View>
+        );
+    }
 }
 
 export default App;
